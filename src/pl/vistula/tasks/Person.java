6dd01df7a3d7d8ee66ee7fbb37e3eb353807035c
Migisha098 <100316803+Migisha098@users.noreply.github.com>
2022-04-18 03:48:22 +0200

    initial commit

--- src/pl/vistula/tasks/Person.java
+++ src/pl/vistula/tasks/Person.java
@@ -4,7 +4,26 @@ package pl.vistula.tasks;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-    public class Person {
+
+interface salary {
+    public void salaryToPayFabrine57098(int hoursFabrine57098, int rateFabrine57098, int computeSalary57098);
+
+    default int salaryOvertimeFabrine57098(int hoursFabrine57098, int rateFabrine57098) {
+        int computeSalary57098 = hoursFabrine57098 * rateFabrine57098;
+        return computeSalary57098;
+    };
+    static int bonusFabrine57098(int yearsOfExperienceFabrine57098 ) {
+
+        if (yearsOfExperienceFabrine57098 > 5) {
+            return 100;
+
+        } else {
+            return 0;
+        }
+    };
+
+
+     public class Person{
         String nameFabrine57098;
         String surnameFabrine57098 ;
         String streetFabrine57098 ;
@@ -37,10 +56,14 @@ import java.io.InputStreamReader;
 
         }
 
+         public int salaryToPayFabrine57098(int i, int i1) {
+             return 0;
+         }
 
-        public class Staff {
+
+         public static class Staff {
         }
-    }class Staff extends Person{
+    }class Staff extends Person implements salary{
         String education;
         String position;
 
@@ -60,6 +83,16 @@ import java.io.InputStreamReader;
 
 
         }
+
+        @Override
+        public void salaryToPayFabrine57098(int hoursFabrine57098, int rateFabrine57098, int computeSalary57098) {
+
+        }
+
+        @Override
+        public int salaryOvertimeFabrine57098(int hoursFabrine57098, int rateFabrine57098) {
+            return salary.super.salaryOvertimeFabrine57098(hoursFabrine57098, rateFabrine57098);
+        }
     }
 
 class HR{
@@ -68,5 +101,33 @@ class HR{
 
         employee.initialize1();
         employee.print1();
+
+        int salary = employee.salaryToPayFabrine57098(150,30);
+        System.out.println("The Employee received a salary in the amount of"+ salary);
+
+        int salaryOvertime = employee.salaryOvertimeFabrine57098(12 ,75 );
+        System.out.println("The Employee received a salary for overtime in the amount of ");
+
+        pl.vistula.tasks.salary Salary;
+        int bonusFabrine57098 = pl.vistula.tasks.salary.bonusFabrine57098(7);
+        System.out.println("The employee received a bonus in the amount of "+ bonusFabrine57098);
     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
